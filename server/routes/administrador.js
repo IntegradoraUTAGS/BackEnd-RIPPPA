8d92e3e1edea1cf7336c186f1d085a180cac9fb2
Valeria <valevillanela20@gmail.com>
2020-02-27 14:55:09 -0600

    Se corrigieron errores

--- server/routes/administrador.js
+++ server/routes/administrador.js
@@ -4,7 +4,25 @@ const _ = require('underscore');
 const app = express();
 
 app.get('/administrador/obtener', (req, res) => {
-    Administrador.find({ estado: true }) //select * from usuario where estado=true
+    Administrador.find({ estado: true }).populate('idRol').populate('idDireccion') //select * from usuario where estado=true
+        //solo aceptan valores numericos
+        .exec((err, administradores) => { //ejecuta la funcion
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            return res.status(200).json({
+                ok: true,
+                count: administradores.length,
+                administradores
+            });
+        });
+});
+app.get('/administrador/obtener', (req, res) => { 
+    let id= req.params.id;
+    Administrador.find({ _id:id}).populate('idRol').populate('idDireccion') //select * from usuario where estado=true
         //solo aceptan valores numericos
         .exec((err, administradores) => { //ejecuta la funcion
             if (err) {
@@ -21,17 +39,16 @@ app.get('/administrador/obtener', (req, res) => {
             });
         });
 });
-
 app.post('/administrador/registrar', (req, res) => {
     let body = req.body;
     let administrador = new Administrador({
         //para poder mandar los datos a la coleccion
         idRol: body.idRol,
         idDireccion: body.idDireccion,
-        strNnombre: body.strNombre,
+        strNombre: body.strNombre,
         strCodigoEmpleado: body.strCodigoEmpleado,
-        contraseña: bcript.hashSync(body.contraseña, 10),
-        estado: body.estado
+        strContraseña: bcript.hashSync(body.strContraseña, 10),
+        blnEstado: body.blnEstado
     });
     administrador.save((err, admDB) => {
         if (err) {
@@ -48,7 +65,7 @@ app.post('/administrador/registrar', (req, res) => {
 });
 app.put('/administrador/actualizar/:id', (req, res) => {
     let id = req.params.id;
-    let body = _.pick(req.body, ['idRol', 'idDireccion', 'strNombre', 'strCodigoEmpleado', 'contraseña', 'estado']); //FILTRAR del body, on el pick seleccionar los campos que interesan del body 
+    let body = _.pick(req.body, ['idRol', 'idDireccion', 'strNombre', 'strCodigoEmpleado', 'strContraseña', 'blnEstado']); //FILTRAR del body, on el pick seleccionar los campos que interesan del body 
     //id 'su coleccion, new -> si no existe lo inserta, runVali-> sirve para validar todas las condiciones del modelo 
     Administrador.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, admDB) => {
         if (err) {
@@ -67,13 +84,12 @@ app.put('/administrador/actualizar/:id', (req, res) => {
 app.delete('/administrador/eliminar/:id', (req, res) => {
     let id = req.params.id;
 
-    Usuario.findByIdAndUpdate(id, { estado: false }, { new: true, runValidators: true, context: 'query' }, (err, resp) => {
+    Usuario.findByIdAndUpdate(id, { blnEstado: false }, { new: true, runValidators: true, context: 'query' }, (err, resp) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
                 err
             });
-
         }
         return res.status(200).json({
             ok: true,
