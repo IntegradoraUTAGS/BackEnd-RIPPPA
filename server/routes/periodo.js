7f0833293e89421b7ca3db86226e6974bb9edd17
Natanael Acero <nataacero1@gmail.com>
2020-02-28 10:03:44 -0600

    Se realizó la API de periodos

---
+++ server/routes/periodo.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const _ = require('underscore');
+const { verificaToken } = require('../middlewares/autenticacion');
+const periodo = require('../models/periodo');
+const app = express();
+
+app.get('/periodos/obtener', (req, res) => {
+    periodo.find({ blnEstado: true })
+        .exec((err, periodos) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            return res.status(200).json({
+                ok: true,
+                count: periodos.length,
+                periodos
+            });
+        });
+});
+app.get('/periodos/obtener/:id', (req, res) => {
+    let id = req.params.id;
+    periodo.find({ blnEstado: true, _id: id })
+        .exec((err, periodos) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            return res.status(200).json({
+                ok: true,
+                count: periodos.length,
+                periodos
+            });
+        });
+});
+
+app.post('/periodos/registrar', (req, res) => {
+    let body = req.body;
+    let Periodo = new periodo({
+        strPeriodo: body.strPeriodo
+    });
+    Periodo.save((err, perDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        return res.status(200).json({
+            ok: true,
+            perDB
+        });
+    });
+});
+
+app.put('/periodos/actualizar/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['strPeriodo']);
+    periodo.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, perDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        return res.status(200).json({
+            ok: true,
+            perDB
+        });
+
+    });
+});
+
+app.delete('/periodos/eliminar/:id', (req, res) => {
+    let id = req.params.id;
+
+    periodo.findByIdAndUpdate(id, { blnEstado: false }, { new: true, runValidators: true, context: 'query' }, (err, resp) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        return res.status(200).json({
+            ok: true,
+            resp
+        });
+    });
+});
+
+module.exports = app;
