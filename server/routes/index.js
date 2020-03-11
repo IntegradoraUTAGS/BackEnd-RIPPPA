a1e6731f363e5b74a3abbaf1efca9491f625eed7
Natanael Acero <nataacero1@gmail.com>
2020-03-11 14:16:38 -0600

    Se modificaron  las rutas del index

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,13 @@ const app = express();
 
 //importar rutas
 
-app.use(require('./login'));
-app.use('/administrador', require('./administrador'))
-app.use('/direccion', require('./direccion'));
-app.use('/statusConvocatoria', require('./statusConvocatoria'));
-app.use('/menu', require('./menu'));
-app.use('/periodo', require('./periodo'));
-app.use('/academias', require('./academias'))
-app.use('/licenciatura', require('./licenciatura'));
-app.use('/rol', require('./rol'));
+app.use('/administrador', require('./administrador/administrador'))
+app.use('/direccion', require('./direccion/direccion'));
+app.use('/statusConvocatoria', require('./statusConvocatoria/statusConvocatoria'));
+app.use('/menu', require('./menu/menu'));
+app.use('/periodo', require('./periodo/periodo'));
+app.use('/academias', require('./academias/academias'))
+app.use('/licenciatura', require('./licenciatura/licenciatura'));
+app.use('/rol', require('./rol/rol'));
 
 module.exports = app;
