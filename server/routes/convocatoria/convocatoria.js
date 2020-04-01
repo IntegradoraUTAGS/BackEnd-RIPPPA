3fe4e19cdde8664a2089d285ee7aebf3694b8d1b
Natanael Acero <nataacero1@gmail.com>
2020-04-01 12:53:12 -0600

    Se agregó el modulo de convocatoria

---
+++ server/routes/convocatoria/convocatoria.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const _ = require('underscore');
+const Convocatoria = require('../models/convocatoria');
+
+app.get('/convocatoria', (req, res) => {
+    Convocatoria.find()
+        .exec((err, ) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            return res.status(200).json({
+                ok: true,
+                count: convocatoria.length,
+                convocatorias
+            })
+        });
+});
+app.post('/convocatoria', (req, res) => {
+    let body = req.body;
+
+    let convocatoria = new Convocatoria({
+        idDireccion: body.idDireccion,
+        idProfesor: body.idProfesor,
+        idAcademia: body.idAcademia,
+        idPeriodo: body.idPeriodo,
+        strTurno: body.strTurno,
+        numHoras: body.numHoras
+    });
+
+
+    convocatoria.save((err, conDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        return res.status(200).json({
+            ok: true,
+            conDB
+        });
+    });
+});
+app.put('/convocatoria/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['idDireccion', ' idProfesor', ' idAcademia', ' idPeriodo', 'strTurno', ' numHoras']);
+
+    Licenciatura.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, conDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        } else {
+            return res.status(200).json({
+                ok: true,
+                conDB
+            });
+        }
+    });
+});
+app.delete('/convocatoria/:id', (req, res) => {
+    let id = req.params.id;
+
+    Licenciatura.findByIdAndUpdate(id, { disponible: false }, { new: true, runValidators: true, context: 'query' }, (err, resp) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        return res.status(200).json({
+            ok: true,
+            conDB
+        });
+    });
+});
+module.exports = app;
